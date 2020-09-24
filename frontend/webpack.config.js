be7cc94ade9bd7e8d247148ac8f09111899c677e
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-09-24 19:39:03 +1000

    Hopefully make webpack load fonts?

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -26,7 +26,19 @@ let mainConfig  = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
-            }
+            },
+          {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts/'
+                }
+              }
+            ]
+          }
         ]
     },
     resolve: {
